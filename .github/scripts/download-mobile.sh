f640369e976b6b19069d8bdf6ecdc60c17b408b3
阿乔 <68536499+HenryChiao@users.noreply.github.com>
2026-01-30 21:52:47 +0800

    Update download-mobile.sh

--- .github/scripts/download-mobile.sh
+++ .github/scripts/download-mobile.sh
@@ -4,10 +4,10 @@ source "$(dirname "$0")/lib_fetch.sh"
 echo "📦 Processing Mobile Modules..."
 
 TASKS=$(cat <<'EOF'
-https://raw.githubusercontent.com/GitMetaio/Surfing/refs/heads/main/box_bll/clash/config.yaml|Mobile_Modules/Surfing/config.yaml
-https://raw.githubusercontent.com/akashaProxy/akashaProxy/refs/heads/master/module/src/config.example.yaml|Mobile_Modules/AkashaProxy/config.yaml
-https://raw.githubusercontent.com/AXEVO/Clash-MIX/refs/heads/Clash-MIX-4.0/Clash/Clash%E9%85%8D%E7%BD%AE.yaml|Mobile_Modules/ClashMix/config.yaml
-https://raw.githubusercontent.com/boxproxy/box/refs/heads/master/box/mihomo/config.yaml|Mobile_Modules/BoxProxy/config.yaml
+https://raw.githubusercontent.com/GitMetaio/Surfing/refs/heads/main/box_bll/clash/config.yaml|THEYAMLS/Mobile_Modules/Surfing/config.yaml
+https://raw.githubusercontent.com/akashaProxy/akashaProxy/refs/heads/master/module/src/config.example.yaml|THEYAMLS/Mobile_Modules/AkashaProxy/config.yaml
+https://raw.githubusercontent.com/AXEVO/Clash-MIX/refs/heads/Clash-MIX-4.0/Clash/Clash%E9%85%8D%E7%BD%AE.yaml|THEYAMLS/Mobile_Modules/ClashMix/config.yaml
+https://raw.githubusercontent.com/boxproxy/box/refs/heads/master/box/mihomo/config.yaml|THEYAMLS/Mobile_Modules/BoxProxy/config.yaml
 EOF
 )
 
